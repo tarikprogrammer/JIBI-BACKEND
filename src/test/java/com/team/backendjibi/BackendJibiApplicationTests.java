db4bd78f7665f51039e0d835d52eeea17bddfc42
tarikprogrammer <belaid.tarikk@gmail.com>
2024-05-04 21:35:31 +0100

    first push

---
+++ src/test/java/com/team/backendjibi/BackendJibiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.backendjibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJibiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
