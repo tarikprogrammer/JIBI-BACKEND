0b73f6c2a43fcd67c66df9370cbe85eda1919865
tarikbelaid <150623539+tarikprogrammer@users.noreply.github.com>
2024-05-23 16:24:07 +0100

    Delete src/main/java/com/team/backendjibi/CMI directory

--- src/main/java/com/team/backendjibi/CMI/repository/CreancierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team.backendjibi.CMI.repository;
-
-import com.team.backendjibi.CMI.entity.Creancier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CreancierRepository extends JpaRepository<Creancier , Long> {
-}
