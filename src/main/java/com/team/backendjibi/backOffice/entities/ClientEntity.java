bb05bb23ca6b04bcee4d3a56769e5653a4a39c6a
tarikprogrammer <belaid.tarikk@gmail.com>
2024-06-10 15:56:57 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/team/backendjibi/backOffice/entities/ClientEntity.java

--- src/main/java/com/team/backendjibi/backOffice/entities/ClientEntity.java
+++ src/main/java/com/team/backendjibi/backOffice/entities/ClientEntity.java
@@ -1,5 +1,6 @@
 package com.team.backendjibi.backOffice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.team.backendjibi.backOffice.profiles.ClientProfile;
 import com.team.backendjibi.cmi.entity.Account;
 import com.team.backendjibi.cmi.entity.HistoriquesEntity;
@@ -30,11 +31,20 @@ public class ClientEntity {
     @ToString.Exclude
     @OneToOne(mappedBy = "client",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private ClientProfile clientProfile;
+
+    @ToString.Exclude
     @OneToOne(mappedBy = "client",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private Account account;
+
+    @ToString.Exclude
     @OneToOne(mappedBy = "client",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private OtpEntity otpClient;
+
+    @ToString.Exclude
     @OneToMany(mappedBy = "client",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private List<HistoriquesEntity> historiques;
+
+
+
 
 }
